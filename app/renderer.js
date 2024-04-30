a8f34b545c4dd2c7d2169696edcc7c4e211a8f55
zero-jedynkowy <01110010pl@gmail.com>
2024-04-30 20:34:22 +0200

    Cleaning up the js code

--- app/renderer.js
+++ app/renderer.js
@@ -6,131 +6,122 @@ window.$ = window.jQuery = require('jquery');
 const { SerialPort } = require('serialport')
 const fs = require('fs');
 const path = require('path');
-const { equal } = require('assert');
 const settings = currentWindow.settings;
 
-
 // SETTINGS
-
-function setSettings()
 {
-    if(settings.getSync("theme") == "dark")
+    function setSettings()
     {
-        changeTheme({target:{id: "darkThemeSwitcher"}})
-        document.querySelector("#darkThemeSwitcher").setAttribute("checked", "")
+        if(settings.getSync("theme") == "dark")
+        {
+            changeTheme({target:{id: "darkThemeSwitcher"}})
+            document.querySelector("#darkThemeSwitcher").setAttribute("checked", "")
+        }
+        else
+        {
+            changeTheme({target:{id: "lightThemeSwitcher"}})
+            document.querySelector("#lightThemeSwitcher").setAttribute("checked", "")
+        }
+        if(settings.getSync("language") == "english")
+        {
+            changeLanguage({target: {id: "languageSwitcher2"}})
+            document.querySelector("#languageSwitcher2").setAttribute("checked", "")
+        }
+        else
+        {
+            changeLanguage({target: {id: "languageSwitcher1"}})
+            document.querySelector("#languageSwitcher1").setAttribute("checked", "")
+        }
     }
-    else
+
+    function changeTheme(event)
     {
-        changeTheme({target:{id: "lightThemeSwitcher"}})
-        document.querySelector("#lightThemeSwitcher").setAttribute("checked", "")
+        if(event.target.id == "darkThemeSwitcher")
+        {
+            document.querySelector("html").setAttribute("data-bs-theme", "dark")
+            settings.setSync("theme", "dark")
+        }
+        else
+        {
+            document.querySelector("html").removeAttribute("data-bs-theme")
+            settings.setSync("theme", "light")
+        }
     }
-    if(settings.getSync("language") == "english")
+
+    function switchDevMode(event)
     {
-        changeLanguage({target: {id: "languageSwitcher2"}})
-        document.querySelector("#languageSwitcher2").setAttribute("checked", "")
+        if(event.target.id == "devToolsOnSwitcher")
+        {
+            currentWindow.webContents.openDevTools()
+            $("#consoleSwitcher").fadeIn(1000);
+            currentWindow.resizable = true
+        }
+        else
+        {
+            currentWindow.webContents.closeDevTools()
+            currentWindow.unmaximize()
+            $("#consoleSwitcher").fadeOut(1000);
+            currentWindow.setSize(800, 700)
+            currentWindow.resizable = false
+        }
     }
-    else
+
+    function changeLanguage(event)
     {
-        changeLanguage({target: {id: "languageSwitcher1"}})
-        document.querySelector("#languageSwitcher1").setAttribute("checked", "")
+        let content;
+        let myPath;
+        if(event.target.id == "languageSwitcher1")
+        {
+            myPath = path.join(__dirname, 'languages', 'polski.json');
+            settings.setSync("language", "polski")
+        }
+        else
+        {
+            myPath = path.join(__dirname, 'languages', 'english.json');
+            settings.setSync("language", "english")
+        }
+        let rawData = fs.readFileSync(myPath,  { encoding: 'utf8', flag: 'r' })
+        let myData = JSON.parse(rawData)
+        
+        let components = document.querySelectorAll(".language")
+        for(let i=0; i<components.length; i++)
+        {
+            components[i].innerHTML = myData[components[i].id]
+            console.log(components[i].innerHTML)
+        }
     }
 }
-
-setSettings()
 
 // OTHER
-
-function setLeftPanelHeight()
 {
-    let temp = document.querySelector("#portsListTitleParent").offsetHeight
-    temp = document.querySelector("body").offsetHeight - temp
-    document.querySelector(".leftPanel").style.height = temp + "px"
+    function setLeftPanelHeight()
+    {
+        let temp = document.querySelector("#portsListTitleParent").offsetHeight
+        temp = document.querySelector("body").offsetHeight - temp
+        document.querySelector(".leftPanel").style.height = temp + "px"
+    }
 }
 
+// CONNECT
+{
+    function connectToDevice()
+    {
+
+    }
+}
+
+// GENERAL THINGS
 $(document).ready(function()
 { 
+    setSettings()
+    document.querySelector("#languageSwitcher2").addEventListener("click", changeLanguage)
+    document.querySelector("#languageSwitcher1").addEventListener("click", changeLanguage)
+    document.querySelector("#consoleSwitcher").addEventListener("click", (event) => {currentWindow.webContents.toggleDevTools()})
+    document.querySelector("#devToolsOnSwitcher").addEventListener("click", switchDevMode);
+    document.querySelector("#devToolsOffSwitcher").addEventListener("click", switchDevMode);
+    document.querySelector("#connectButton").addEventListener("click", connectToDevice);
+    document.querySelector("#darkThemeSwitcher").addEventListener("click", changeTheme)
+    document.querySelector("#lightThemeSwitcher").addEventListener("click", changeTheme)
     $("#chooseDeviceFrame").fadeIn(1000)
     setLeftPanelHeight()
 });
-
-// CONNECT TO DEVICE
-
-function connectToDevice()
-{
-
-}
-
-document.querySelector("#connectButton").addEventListener("click", connectToDevice);
-
-// CHANGE THEME
-
-function changeTheme(event)
-{
-    if(event.target.id == "darkThemeSwitcher")
-    {
-        document.querySelector("html").setAttribute("data-bs-theme", "dark")
-        settings.setSync("theme", "dark")
-    }
-    else
-    {
-        document.querySelector("html").removeAttribute("data-bs-theme")
-        settings.setSync("theme", "light")
-    }
-}
-
-document.querySelector("#darkThemeSwitcher").addEventListener("click", changeTheme)
-document.querySelector("#lightThemeSwitcher").addEventListener("click", changeTheme)
-
-// DEV MODE
-
-function switchDevMode(event)
-{
-    if(event.target.id == "devToolsOnSwitcher")
-    {
-        currentWindow.webContents.openDevTools()
-        $("#consoleSwitcher").fadeIn(1000);
-        currentWindow.resizable = true
-    }
-    else
-    {
-        currentWindow.webContents.closeDevTools()
-        currentWindow.unmaximize()
-        $("#consoleSwitcher").fadeOut(1000);
-        currentWindow.setSize(800, 700)
-        currentWindow.resizable = false
-    }
-}
-
-document.querySelector("#consoleSwitcher").addEventListener("click", (event) => {currentWindow.webContents.toggleDevTools()})
-document.querySelector("#devToolsOnSwitcher").addEventListener("click", switchDevMode);
-document.querySelector("#devToolsOffSwitcher").addEventListener("click", switchDevMode);
-
-//language
-
-function changeLanguage(event)
-{
-    let content;
-    let myPath;
-    if(event.target.id == "languageSwitcher1")
-    {
-        myPath = path.join(__dirname, 'languages', 'polski.json');
-        settings.setSync("language", "polski")
-    }
-    else
-    {
-        myPath = path.join(__dirname, 'languages', 'english.json');
-        settings.setSync("language", "english")
-    }
-    let rawData = fs.readFileSync(myPath,  { encoding: 'utf8', flag: 'r' })
-    let myData = JSON.parse(rawData)
-    
-    let components = document.querySelectorAll(".language")
-    for(let i=0; i<components.length; i++)
-    {
-        components[i].innerHTML = myData[components[i].id]
-        console.log(components[i].innerHTML)
-    }
-}
-
-document.querySelector("#languageSwitcher2").addEventListener("click", changeLanguage)
-document.querySelector("#languageSwitcher1").addEventListener("click", changeLanguage)
