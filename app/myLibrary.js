ec0024c5928d7c14b573a0ed325db6b96ca1b88c
zero-jedynkowy <01110010pl@gmail.com>
2024-05-27 08:34:59 +0200

    Working reading data from the device MCU

---
+++ app/myLibrary.js
@@ -0,0 +1,360 @@
+const remote = window.require("@electron/remote");
+const currentWindow = remote.getCurrentWindow();
+const bootstrap = require('bootstrap');
+const { connectToDevice } = require('./myDeviceConnection');
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline')
+window.$ = window.jQuery = require('jquery');
+
+//OTHER
+function addContent(filePath, destination)
+{
+    let data = fs.readFileSync(filePath)
+    document.querySelector(destination).innerHTML = document.querySelector(destination).innerHTML + data
+}
+
+//MENU
+menuObj = 
+{
+    menuIsActive: true,
+    isMenuAnimationActive: false
+}
+
+function showMenuButtonAction()
+{
+    if(menuObj.isMenuAnimationActive == false)
+    {
+        if(!menuObj.menuIsActive)
+        {
+            menuObj.isMenuAnimationActive = true
+            $("#sideBar").animate({"left":"0px"}, 500, () => {menuObj.isMenuAnimationActive = 0})
+        }
+        else
+        {
+            menuObj.isMenuAnimationActive = true
+            $("#sideBar").animate({"left": "-" + $("#sideBar").outerWidth().toString() + "px"}, 500, () => {menuObj.isMenuAnimationActive = 0});
+        }
+        menuObj.menuIsActive = menuObj.menuIsActive? false:true
+    }
+}
+
+
+//WINDOW
+
+function resizeWindowUpdater()
+{
+    let size = currentWindow.getSize();
+    let channels = document.querySelectorAll(".channel")
+    if(size[0] < 1452)
+    {
+        
+        $(".channelsRows").addClass("flex-column")
+        $(".channelsRows").removeClass("flex-row")
+        for(let i=0; i<channels.length; i=i+2) channels[i].classList.remove("me-3")
+    }
+    else
+    {
+        $(".channelsRows").addClass("flex-row")
+        $(".channelsRows").removeClass("flex-column")
+        for(let i=0; i<channels.length; i=i+2) channels[i].classList.add("me-3")
+    }
+    if(size[0] < 1000)
+    {
+        $("#showMenuButton").fadeIn(250)
+        $("#menuPlaceHolder").css("width", "0");
+        $("#mainContent").css("width", "100%");
+        $("#sideBar").css("width", "40%");
+        if(menuObj.menuIsActive)
+        {
+            $("#sideBar").css("left", "0px")
+            menuObj.menuIsActive = 1;
+        }
+        else $("#sideBar").css("left", "-" + $("#sideBar").outerWidth().toString() + "px");
+    }
+    else
+    {
+        $("#showMenuButton").fadeOut(250)
+        $("#menuPlaceHolder").css("width", "27%");
+        $("#mainContent").css("width", "73%");
+        $("#sideBar").css("width", "27%");
+        $("#sideBar").css("left", "0px");
+    }   
+    $("#mainContent").height(currentWindow.getSize()[1])
+}
+
+//CONNECTION
+
+connectionObj = 
+{
+    portsKeys: [],
+    ports: [],
+    refreshing: true,
+    port: null,
+    parser: null,
+    receivingData: null,
+    receivedDataDate: null,
+    connected: false,
+    connectingCounter: 0,
+    myConnectionInterval: null
+}
+
+function waitForConnecting()
+{
+    connectionObj.connectingCounter++;
+    if(connectionObj.connectingCounter >= 10)
+    {
+        if($("#connectingModal").is(":visible")) $("#connectingModal").modal('hide')
+        else
+        {
+            connectionObj.connectingCounter = 0;
+            connectionObj.port = null
+            startRefreshing()
+            connectionObj.connected = false
+            switchDevicePanel(false)
+            $("#errorConnectingModal").modal('show')
+            clearInterval(connectionObj.myConnectionInterval)
+        }
+
+    }
+}
+
+function getCurrentTime() 
+{
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+    return `${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
+
+function markDevice(event)
+{
+    let x = document.querySelector(".marked")
+    if(event.target.classList.contains("marked"))
+    {
+        x.classList.remove("marked")
+        x.classList.remove("bg-success-subtle")
+    }
+    else
+    {
+        if(x != null)
+        {
+            x.classList.remove("marked")
+            x.classList.remove("bg-success-subtle")
+        }
+        event.target.classList.add("marked")
+        event.target.classList.add("bg-success-subtle")
+    }
+}
+
+function toggleDevicesList(switcher)
+{
+    $("#devicesListContent").removeClass('d-none')
+    if(switcher) $("#devicesListStatePlaceholderTitle").parent().fadeOut(500, () => {$("#devicesListContent").fadeIn(1000)})
+    else $("#devicesListContent").fadeOut(500, () => {$("#devicesListStatePlaceholderTitle").parent().fadeIn(1000)})
+}
+
+function refreshDeviceList()
+{
+    if($("#connectingModal").is(":visible")) $("#connectingModal").modal('hide')
+    if($("#devicePanel").is(":visible")) $("#devicePanel").parent().fadeOut()
+    SerialPort.list().then((newPorts) =>
+    {
+        let newPortsKeys = []
+        let tempPortName = null
+        for(let i=0; i<newPorts.length; i++)
+        {
+            let tempPortName = newPorts[i].path
+            newPortsKeys.push(tempPortName)
+            if(!connectionObj.portsKeys.includes(tempPortName))
+            {
+                connectionObj.portsKeys.unshift(tempPortName)
+                connectionObj.ports.unshift(newPorts[i])
+                let tempObj = document.createElement("li")
+                tempObj.classList.add("list-group-item")
+                tempObj.classList.add("deviceListElement")
+                tempObj.classList.add("p-3")
+                tempObj.innerHTML = tempPortName
+                tempObj.addEventListener("click", markDevice)
+                let temp = document.querySelectorAll(".deviceListElement")
+                if(temp.length == 0)
+                {
+                    $(tempObj).hide().appendTo("#devicesListContent").fadeIn();
+                }
+                else
+                {
+                    $(tempObj).hide().insertBefore(temp[0]).fadeIn(500);
+                }
+            }
+        }
+        
+        for(let i=0; i<connectionObj.ports.length; i++)
+        {
+            if(!newPortsKeys.includes(connectionObj.portsKeys[i]))
+            {
+                connectionObj.ports.splice(i, 1);
+                connectionObj.portsKeys.splice(i, 1);
+                $(document.querySelectorAll(".deviceListElement")[i]).fadeOut(500, () => $(document.querySelectorAll(".deviceListElement")[i]).remove())
+            }
+        }
+        if(connectionObj.ports.length == 0 && $("#devicesListContent").is(":visible")) toggleDevicesList(true)
+        else if(connectionObj.ports.length != 0 && $("#devicesListContent").is(":hidden")) toggleDevicesList(true)
+        if(connectionObj.refreshing) refreshDeviceList()
+    })
+}
+
+function startRefreshing()
+{
+    connectionObj.refreshing = true
+    refreshDeviceList()
+}
+
+function stopRefreshing()
+{
+    connectionObj.refreshing = false
+}
+    
+function openSerialPort(path)
+{
+    return new Promise((resolve, reject) => 
+    {
+        port = new SerialPort({path: path, baudRate: 9600 }, (err) => 
+        {
+            if (err) 
+            {
+                return reject(err);
+            }
+            resolve(port);
+        });
+    });
+}
+
+async function connectActionButton()
+{
+    
+    if(connectionObj.connected)
+    {
+        $("#alreadyConnectedModal").modal("show"); return;
+    }
+    if(document.querySelector(".marked") == null) $("#notChoosenAnyDeviceModal").modal('show')
+    else
+    {
+        try
+        {
+            connectionObj.firstConnection = true
+            connectionObj.port = await openSerialPort(document.querySelector(".marked").innerHTML)
+            stopRefreshing()
+            toggleDevicesList(false)
+            $("#connectingModal").modal('show')
+            connectionObj.parser = port.pipe(new ReadlineParser({ delimiter: '}}', includeDelimiter: true}));
+            setListeners()
+            connectionObj.port.write('{"request":"GET_DATA"}')
+            connectionObj.myConnectionInterval = setInterval(waitForConnecting, 1000)
+        }
+        catch(err)
+        {
+            console.log(err)
+            connectionObj.connected = false
+            connectionObj.port = null
+            $("#errorConnectingModal").modal('show')
+        }
+    }
+}
+
+function disconnectActionButton()
+{
+    if(!connectionObj.connected) $("#notConnectedAnyDeviceModal").modal('show')
+    if(connectionObj.port.isOpen) connectionObj.port.close()
+    connectionObj.port.removeAllListeners()
+    startRefreshing()
+    connectionObj.connected = false
+    switchDevicePanel(false)
+    $("#disconnectingModal").modal("show")
+}
+
+function switchDevicePanel(switcher)
+{
+    if(switcher)
+    {
+        $("#notConnectedToAnyDevicePlaceholder").parent().parent().fadeOut(500, () => {
+            $("#devicePanel").parent().fadeIn(500)
+        })
+        
+    }
+    else
+    {
+        $("#devicePanel").parent().fadeOut(500, () => {
+            $("#notConnectedToAnyDevicePlaceholder").parent().parent().fadeIn(500)
+        })
+    }        
+}
+
+function processReceivedData()
+{
+    $("#deviceIdField").text(connectionObj.receivingData.deviceName)
+    $("#deviceVersionField").text(connectionObj.receivingData.deviceVersion)
+    $("#lastUpdateField").text(connectionObj.receivedDataDate)
+}
+
+function setListeners()
+{
+    
+    connectionObj.port.on('close', (err) => 
+    {
+        try
+        {
+            if(err.disconnected)
+            {
+                connectionObj.port = null
+                $("#connectingModal").modal('hide')
+                startRefreshing()
+                connectionObj.connected = false
+                switchDevicePanel(false)
+                $("#errorConnectingModal").modal('show')
+            }
+        }
+        catch(error)
+        {
+
+        }
+    });
+    
+    connectionObj.parser.on('data', (data) => 
+    {
+        connectionObj.receivingData = JSON.parse(data)
+        connectionObj.receivedDataDate = getCurrentTime()
+        console.log(connectionObj.receivedDataDate)
+        console.log(connectionObj.receivingData);
+        if(connectionObj.receivingData.deviceName == 'Mass Flow Controller Device Prototype')
+        {
+            clearInterval(connectionObj.myConnectionInterval)
+            processReceivedData()
+            if($("#connectingModal").is(":visible")) $("#connectingModal").modal('hide')
+                $("#connectingModal").on('hide.bs.modal', function(){
+                    switchDevicePanel(true)
+                    connectionObj.connected = true
+                    });
+            
+        }
+        
+        setTimeout(() => {port.write('{"request":"GET_DATA"}', function(err) {console.log(err)})}, 1000)
+    });
+}
+
+
+module.exports = 
+{
+    
+    addContent,
+    showMenuButtonAction,
+    resizeWindowUpdater,
+    refreshDeviceList,
+    toggleDevicesList,
+    connectActionButton,
+    stopRefreshing,
+    disconnectActionButton,
+    startRefreshing,
+    switchDevicePanel
+    
+}
--- app/renderer.js
+++ app/renderer.js
@@ -7,52 +7,44 @@ const fs = require('fs');
 const path = require('path');
 const shell = require('electron').shell;
 
-const myOthers = require('./myOthers')
-const mySettings = require('./mySettings')
-const myDeviceConnection = require('./myDeviceConnection')
 
-function addContent(filePath, destination)
-{
-    let data = fs.readFileSync(filePath)
-    document.querySelector(destination).innerHTML = document.querySelector(destination).innerHTML + data
-}
+const myLibrary = require('./myLibrary')
 
 
 
-mySettings.initSettings()
-currentWindow.on("resize", myOthers.resizeWindowUpdater)
-currentWindow.on("maximize", myOthers.resizeWindowUpdater)
-
-
+currentWindow.on("resize", myLibrary.resizeWindowUpdater)
+currentWindow.on("maximize", myLibrary.resizeWindowUpdater)
 
 $(document).ready(function()
 { 
-    addContent('window.html', '#bodyContent')
-    addContent('mainContent.html', '#mainContent')
-    addContent("menu.html", "#menuAndContentContainer")
-    addContent('buttons.html', "#bodyContent")
-    addContent('modals.html', "#bodyContent")
+    myLibrary.addContent('window.html', '#bodyContent')
+    myLibrary.addContent('mainContent.html', '#mainContent')
+    myLibrary.addContent("menu.html", "#menuAndContentContainer")
+    myLibrary.addContent('buttons.html', "#bodyContent")
+    myLibrary.addContent('modals.html', "#bodyContent")
     
 
-    $("#showMenuButton").click(myOthers.showMenuButtonAction)
-    $('#changeThemeButton').click(mySettings.changeTheme)
-    $('[data-bs-toggle="tooltip"]').tooltip({trigger : 'hover'}) 
-    $("#devModeButton").click(mySettings.switchDevMode)
-    $("#toggleConsoleButton").click(mySettings.showConsole)
-    $("#changeLanguageButton").click(mySettings.changeLanguageButtonAction)
-    myOthers.resizeWindowUpdater()
-    myDeviceConnection.startLoopRefreshDeviceList()
-    $("#moreSettingsButton").click(() => {$('#extraSettingsModal').modal('toggle')})
-    $("#aboutProgramButton").click(() => {$("#aboutProgramModal").modal("toggle")})
-    $("#channelsChartSwitcher").click(mySettings.switchChannelsChartButtonAction)
-    // mySettings.applySettings()
-    $("#connectButton").click(myDeviceConnection.connectToDevice)
-    $(document).on('click', 'a[href^="http"]', function(event) {
-        event.preventDefault();
-        shell.openExternal(this.href);
-    });
-
-    
-    $("body").fadeIn(1000)
+    $("#showMenuButton").click(myLibrary.showMenuButtonAction)
+    $("#connectButton").click(myLibrary.connectActionButton)
+    $("#disconnectButton").click(myLibrary.disconnectActionButton)
+    // $('#changeThemeButton').click(mySettings.changeTheme)
+    // $('[data-bs-toggle="tooltip"]').tooltip({trigger : 'hover'}) 
+    // $("#devModeButton").click(mySettings.switchDevMode)
+    // $("#toggleConsoleButton").click(mySettings.showConsole)
+    // $("#changeLanguageButton").click(mySettings.changeLanguageButtonAction)
+    // myOthers.resizeWindowUpdater()
+    // myDeviceConnection.startLoopRefreshDeviceList()
+    // $("#moreSettingsButton").click(() => {$('#extraSettingsModal').modal('toggle')})
+    // $("#aboutProgramButton").click(() => {$("#aboutProgramModal").modal("toggle")})
+    // $("#channelsChartSwitcher").click(mySettings.switchChannelsChartButtonAction)
+    // // mySettings.applySettings()
+    // $("#connectButton").click(myDeviceConnection.connectToDevice)
+    // $(document).on('click', 'a[href^="http"]', function(event) {
+    //     event.preventDefault();
+    //     shell.openExternal(this.href);
+    // });
+    $("#devicePanel").parent().hide()
+    myLibrary.resizeWindowUpdater()
+    $("body").fadeIn(1000, () => {myLibrary.startRefreshing()})
 });
 
